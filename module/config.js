ea8fdc0982ec0dd0ffd4b7a071122db9631eeb71
YoimPouet <65048905+YoimPouet@users.noreply.github.com>
2023-04-05 20:13:22 +0200

    Pouvoir
    -Ajout des déroulants
    -Reset de la db Pouvoirs

--- module/config.js
+++ module/config.js
@@ -1,12 +1,77 @@
 export const shaanRenaissance = {};
 
-shaanRenaissance.acquis = {
+shaanRenaissance.skills = {
     none: "",
-    Initiative: "InitiativeLabel",
-    acquisBonus: "shaanRenaissance.ACQUIS.acquisBonus",
-    acquisEffect: "shaanRenaissance.ACQUIS.acquisEffect",
-    price: "shaanRenaissance.ACQUIS.price",
-    trihnEsprit: "shaanRenaissance.ACQUIS.trihnEsprit",
-    trihnAme: "shaanRenaissance.ACQUIS.trihnAme",
-    trihnCorps: "shaanRenaissance.ACQUIS.trihnCorps"
+    Technique: "SRdomains.Technique",
+    Savoir: "SRdomains.Savoir",
+    Social: "SRdomains.Social",
+    Arts: "SRdomains.Arts",
+    Shaan: "SRdomains.Shaan",
+    Magie: "SRdomains.Magie",
+    Rituels: "SRdomains.Rituels",
+    Survie: "SRdomains.Survie",
+    Combat: "SRdomains.Combat",
+    Nécrose: "SRdomains.Nécrose",
+    }
+shaanRenaissance.pouvoirs = {
+    pouvoirSelect: {
+        none: "",
+        AstucedeTechnique: "Astuce de Technique",
+        SecretdeSavoir: "Secret de Savoir",
+        PrivilegedeSocial: "Privilège de Social",
+        CréationdArts: "Création d'Arts",
+        SymbiosedeShaan: "Symbiose de Shaan",
+        SortdeMagie: "Sort de Magie",
+        TransedeRituel: "Transe de Rituel",
+        ExploitdeSurvie: "Exploit de Survie",
+        TactiquedeCombat: "Tactique de Combat",
+        TourmentdeNécrose: "Tourment de Nécrose"
+    },
+    pouvoirRank: {
+        none: "",
+        Rank1: "Rang 1",
+        Rank2: "Rang 2",
+        Rank3: "Rang 3",
+        Rank4: "Rang 4",
+    },
+    pouvoirType: {
+        none: "",
+        attaque: "Attaque",
+        défense: "Défense",
+        déplacement: "Déplacement",
+        amélioration: "Amélioration",
+        altération: "Altération",
+        invocation: "Invocation",
+        controle: "Contrôle",
+        perception: "Perception",
+        recuperation: "Récupération"
+    },
+    pouvoirFréquence: {
+        none: "",
+        permanente: "Permanente",
+        tour: "1/Tour",
+        situation: "1/Situation",
+        jour: "1/Jour",
+        transition: "1/Transition"
+    },
+    pouvoirActivation: {
+        none: "",
+        geste: "1 Geste",
+        action: "1 Action",
+        actions: "2 Actions",
+        actioncible: "1 Action par Cible",
+        heure: "1 Heure",
+        jour: "1 Jour",
+        transition: "1 Transition",
+        testparaction: "1 Test par Action",
+        testparheure: "1 Test par Heure",
+        testparjour: "1 Test par Jour"
+    },
+    pouvoirPortée: {
+        none: "",
+        contact: "Contact",
+        interaction: "Interaction",
+        distance: "Distance",
+        horizon: "Horizon",
+    }
 }
