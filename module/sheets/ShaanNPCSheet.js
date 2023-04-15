c5e5ce6debd20866c21d7475ed565ff32b622eb5
YoimPouet <65048905+YoimPouet@users.noreply.github.com>
2023-04-15 11:31:16 +0200

    Changement fiche PNJ

--- module/sheets/ShaanNPCSheet.js
+++ module/sheets/ShaanNPCSheet.js
@@ -31,6 +31,9 @@ export default class ShaanNPCSheet extends ActorSheet {
         
             sheetData.pouvoirs = actorData.items.filter(function (item) { return item.type == "Pouvoir" });
 
+            sheetData.data.attributes.hpEsprit.value = (Math.max(sheetData.data.skills.Technique.rank, sheetData.data.skills.Savoir.rank, sheetData.data.skills.Social.rank)) + (Math.min(sheetData.data.skills.Technique.rank, sheetData.data.skills.Savoir.rank, sheetData.data.skills.Social.rank))
+            sheetData.data.attributes.hpAme.value = (Math.max(sheetData.data.skills.Arts.rank, sheetData.data.skills.Shaan.rank, sheetData.data.skills.Magie.rank)) + (Math.min(sheetData.data.skills.Arts.rank, sheetData.data.skills.Shaan.rank, sheetData.data.skills.Magie.rank))
+            sheetData.data.attributes.hpCorps.value = (Math.max(sheetData.data.skills.Rituels.rank, sheetData.data.skills.Survie.rank, sheetData.data.skills.Combat.rank)) + (Math.min(sheetData.data.skills.Rituels.rank, sheetData.data.skills.Survie.rank, sheetData.data.skills.Combat.rank))
 
         console.log(sheetData);
         return await sheetData;
