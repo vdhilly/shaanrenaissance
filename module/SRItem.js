35d522efe8f069120c01b28e4d01b65d871efc20
YoimPouet <65048905+YoimPouet@users.noreply.github.com>
2023-04-17 13:53:37 +0200

    suite trihn invoqué

--- module/SRItem.js
+++ module/SRItem.js
@@ -1,19 +1,3 @@
-export default class SRItem extends Item {
-    async roll() {
+export default class SRTrihns extends Item {
 
-        let chatData = {
-            user: game.user._id,
-            speaker: ChatMessage.getSpeaker()
-        };
-
-        let cardData = {
-            ...this.data,
-            owner: this.actor.id
-        };
-
-        chatData.content = await renderTemplate (this.chatTemplate[this.type], cardData);
-
-        chatData.roll = true;
-        return ChatMessage.create(chatData);
-    }
 }
