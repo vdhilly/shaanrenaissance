660ec5de3d84178c47dfa262850c4e6e69fa7cf8
YoimPouet <65048905+YoimPouet@users.noreply.github.com>
2023-05-30 13:12:44 +0200

    Fix optgroup creationDialog Item

--- shaanRenaissance.js
+++ shaanRenaissance.js
@@ -181,7 +181,7 @@ Hooks.on("renderDialog", ((_dialog, $html) => {
   const element = $html[0];
   if(element.classList.contains("dialog-item-create")) {
     const select = element.querySelector("select[name=type]"),
-    categories = game.i18n.translations.ITEM.CreationDialog.Categories;
+    categories = game.i18n.translations.Item.CreationDialog.Categories;
     select && (select.append(extractOptGroup(select, categories.Acquis, ["Armement", "Armimale", "Manuscrit", "Artefact", "Outil", "Transport", "Technologie", "Richesse", "Protection", "Relation", "Bâtiment"])), select.append(extractOptGroup(select, categories.Personnage, ["Race", "Peuple", "Caste", "Métier"])), select.append(extractOptGroup(select, categories.Autres)), select.querySelector("option").selected = !0)
   }
 }))
