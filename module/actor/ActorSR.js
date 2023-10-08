17dae6afca32c421c50d76711af09797d104a5cd
YoimPouet <65048905+YoimPouet@users.noreply.github.com>
2023-10-08 20:02:22 +0200

    clean up actor

--- module/actor/ActorSR.js
+++ module/actor/ActorSR.js
@@ -76,28 +76,6 @@ export class ActorSR extends Actor {
     const allowed = Hooks.call("modifyTokenAttribute", {attribute, value, isDelta, isBar}, updates);
     return allowed !== false ? this.update(updates) : this;
   }
-  async importFromJSON(json) {
-
-    // Construct a document class to (strictly) clean and validate the source data
-    const doc = new this.constructor(JSON.parse(json), {strict: true});
-    console.log(doc)
-
-    // Treat JSON import using the same workflows that are used when importing from a compendium pack
-    const data = this.collection.fromCompendium(doc, {addFlags: false});
-    console.log(data)
-
-    // Preserve certain fields from the destination document
-    const preserve = Object.fromEntries(this.constructor.metadata.preserveOnImport.map(k => {
-      return [k, foundry.utils.getProperty(this, k)];
-    }));
-    preserve.folder = this.folder?.id;
-    foundry.utils.mergeObject(data, preserve);
-
-    // Commit the import as an update to this document
-    await this.update(data, {diff: false, recursive: false, noHook: true});
-    ui.notifications.info(game.i18n.format("DOCUMENT.Imported", {document: this.documentName, name: this.name}));
-    return this;
-  }
 }
 export const ActorProxySR = new Proxy(ActorSR, {
   construct: (_target, args) => new CONFIG.shaanRenaissance.Actor.documentClasses[args[0].type](...args)
